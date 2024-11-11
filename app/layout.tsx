65ea137d8e823097cf25fe774f87ea4858485475
hanamiya-svg <ochuyanga8@gmail.com>
2024-11-12 00:03:47 +0800

    last

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-
+import { inter } from '@/app/ui/fonts'; 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({
   children,
 }: {
@@ -11,4 +20,8 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
+
+  <div
+  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+/>
 }
